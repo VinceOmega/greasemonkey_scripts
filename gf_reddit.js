d81be51186494c5f08ff39ed26f8cbfb946debbe
VinceOmega <vince.omega@gmail.com>
2016-07-27 21:28:13 -0400

    updated script

--- gf_reddit.js
+++ gf_reddit.js
@@ -6,7 +6,7 @@
 // @author       Larry Stanfield - https://www.linkedin.com/in/larrystanfieldwebdev
 // @match        http://*.gamefaqs.com/boards/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/mootools/1.6.0/mootools-core.min.js
-// @run-at       document-start
+// @run-at       document-end
 // @grant        none
 // ==/UserScript==
 
@@ -14,7 +14,16 @@
     'use strict';
 
     
-    var postNumArr = $$('div.msg_body.newbeta').get('data-msgnum');
-    var url = window.location;
-    console.log(postNumArr[1]);
+    var postNumArr     = $$('div.msg_body.newbeta').get('data-msgnum');
+    var postIDArr      = $$('div.msg_body.newbeta').get('name');
+    var postUserArr    = $$('.post_author b').get('text');
+    var url            = window.location.pathname;
+    var slugs          = url.split('/');
+    
+    console.log(postNumArr[0]);
+    console.log(postIDArr[0]);
+    console.log(postUserArr[0]);
+    console.log(slugs[2]);
+    console.log(slugs[3]);
+    
 })();
