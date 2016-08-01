d3edea2067ae21d3ca6bf34e7461acf8817d391b
Larry Stanfield <Larry Stanfield>
2016-07-31 23:27:14 -0400

    Fixed an issue where the usernames in a topic were being printed out. Should be good to go in those regards.

--- gf_reddit.js
+++ gf_reddit.js
@@ -17,12 +17,40 @@
     var postNumArr     = $$('div.msg_body.newbeta').get('data-msgnum');
     var postIDArr      = $$('div.msg_body.newbeta').get('name');
     var postUserArr    = $$('.post_author b').get('text');
+    var alphaNum       = new RegExp(/([A-Za-z0-9\_]+)/i);
     var url            = window.location.pathname;
     var slugs          = url.split('/');
     
-    console.log(postNumArr[0]);
-    console.log(postIDArr[0]);
-    console.log(postUserArr[0]);
+    //console.log(postNumArr[0]);
+    //console.log(postIDArr[0]);
+    //console.log(postUserArr[0]);
+    
+    console.log('List of Post #s');
+    for(var a in postNumArr){
+      if(!isNaN(postNumArr[a])){
+        console.log(postNumArr[a]);
+     }
+    }
+    
+    console.log('List of IDs');
+    for(var b in postIDArr){
+     if(!isNaN(postIDArr[b])){
+       console.log(postIDArr[b]);
+     }
+    }
+    
+    console.log('List of Username');
+     console.log(alphaNum.test(postUserArr[0]));
+     
+    
+    for(var c in postUserArr){
+     if(alphaNum.test(postUserArr[c]) && typeof(postUserArr[c]) !=  'function'){
+        console.log(postUserArr[c]);
+     }
+    }
+    
+    
+    console.log('Url Slugs');
     console.log(slugs[2]);
     console.log(slugs[3]);
     
