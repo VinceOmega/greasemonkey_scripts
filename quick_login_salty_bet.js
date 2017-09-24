c190e2c98e70a30653b4ff75649ddfb3cb9c1f36
VinceOmega <vince.omega@gmail.com>
2017-09-24 00:50:20 -0400

    removed input submit, added button submit, shit works on salty bet again. FUCKING SUCK IT WEB MASTER!

--- quick_login_salty_bet.js
+++ quick_login_salty_bet.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name        Quick Login for Salty Bet
-// @namespace   http://lambdaweb.awardspace.com/
+// @namespace   http://www.larrystanfield.com/
 // @description Log in quicker faster than you can say "NEVER BET ON DBZ"
 // @include     http://www.saltybet.*
-// @version     1
-// @grant       none
+// @version     1.0.01
+// @grant       unsafeWindow
 // ==/UserScript==
 
+
+(function(){
+   'use strict';
 
 // vars for the container for the modal window and background transparency
 var modalPage = document.createElement('div');
@@ -20,22 +23,29 @@ var formSN = document.createElement('form');
 formSN.setAttribute('id', 'signinform');
 formSN.setAttribute('class', 'fancyform');
 formSN.setAttribute('method', 'post');
-formSN.setAttribute('action', '/authenticate?signin=1');
+formSN.setAttribute('action', 'https://www.saltybet.com/authenticate?signin=1');
 //vars for label for Email
 var spanLabelE = document.createElement('span');
 spanLabelE.setAttribute('class', 'field');
 var spanLabelEText = document.createTextNode('Email:');
 spanLabelE.appendChild(spanLabelEText);
+var buttonSubmit = document.createElement('button');
+buttonSubmit.setAttribute('type', 'submit');
+buttonSubmit.setAttribute('value','Sign In');
+var btnText = document.createTextNode('Sign In');
+buttonSubmit.appendChild(btnText);
 //var for email input field
 var inputEmail = document.createElement('input');
 var tempNameArray = ['id', 'type', 'value', 'name'];
 var tempValueArray = ['email', 'text', '', 'email'];
+
+//functions
 function loopThrough(a , b , c){
 var j = b.length;
 	for(var i  = 0; i < j; i++){
 	a.setAttribute(b[i], c[i]);
 	}
-	
+
 	return a;
 }
 
@@ -70,31 +80,28 @@ tempValueArray = ['hidden', 'signin', 'authenticate'];
 loopThrough(inputHiddenAuth, tempNameArray, tempValueArray);
 // vars for submit button
 var inputSubmit = document.createElement('input');
-inputSubmit.setAttribute('type','submit');
-inputSubmit.setAttribute('value', 'Sign In');
+inputSubmit.setAttribute('type','submit', 'id');
+inputSubmit.setAttribute('value', 'Sign In', 'sign-in-button');
 //add to form
 
 tempValueArray = [];
-tempValueArray = [spanLabelE, inputEmail, spanLabelP, inputPassword, inputHiddenAuth, inputSubmit];
+tempValueArray = [spanLabelE, inputEmail, spanLabelP, inputPassword, inputHiddenAuth, buttonSubmit];
 appendKids(formSN, tempValueArray);
 // add to modal window
 modalContainer.appendChild(formSN);
 //add Container to Page
 modalPage.appendChild(modalContainer);
 
-$(document).ready(function(){
-if($('.graybutton').length = 0) {
-	    
-    }
-else{
-$('.graybutton').attr('href', '#');
-$('.footercenter').after(modalPage);
-$('.modal-page').attr('style', 'position: absolute; top: -380%; left: 80%; display: none;');
+    $(document).ready(function(){
+            $('#header ul.navbar-nav li.nav-text a').attr('href', '#');
+            $('.footercenter').after(modalPage);
+            $('.modal-page').attr('style', 'position: absolute; top: -380%; left: 40%; display: none; width: 300px; height: 100px;');
 
-       $('.graybutton').click(function(){
-	           //$('.modal-page').attr('visibility', 'visible');
-		       $('.modal-page').toggle('drop', 1000);
+           $('#header ul.navbar-nav li.nav-text a').click(function(){
+	       //$('.modal-page').attr('visibility', 'visible');
+		   $('.modal-page').toggle('drop', 1000);
+	        });
+        
+    });
 
-	   });
-}
-});
+})();
